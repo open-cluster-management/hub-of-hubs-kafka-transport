254b1ee7b4d4988ff54cf6725f7ee9df080664e5
maroon <maroonay@gmail.com>
2021-08-23 15:05:02 +0300

    added types

---
+++ types/types.go
@@ -0,0 +1,22 @@
+package types
+
+import "time"
+
+const (
+	// MsgIDKey is the common TransportMessage's ID tag.
+	MsgIDKey = "id"
+	// MsgTypeKey is the common TransportMessage's MsgType tag.
+	MsgTypeKey = "msgType"
+	// MsgVersionKey is the common TransportMessage's Version tag.
+	MsgVersionKey = "version"
+
+	// HeaderSizeKey is the key used in total bundle size header.
+	HeaderSizeKey = "size"
+	// HeaderOffsetKey is the key used in message fragment offset header.
+	HeaderOffsetKey = "fragmentOffset"
+	// HeaderDismantlingTimestamp is the key used in bundle dismantling time header.
+	HeaderDismantlingTimestamp = "dismantlingTime"
+
+	// TimeFormat is the format used to present the bundle's dismantling timestamp.
+	TimeFormat = time.RFC3339
+)
