19cedbad12719436538099c72631cd78dcf9ee04
maroon <maroonay@gmail.com>
2021-08-23 15:05:38 +0300

    refactored and added headers to message builder

---
+++ kafka-client/kafka-producer/kafka_message_builder.go
@@ -0,0 +1,46 @@
+package kafkaproducer
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+// KafkaMessageBuilder uses the builder patten to construct kafka messages.
+type KafkaMessageBuilder struct {
+	m kafka.Message
+}
+
+// Build returns the filled kafka message.
+func (b *KafkaMessageBuilder) Build() *kafka.Message {
+	return &b.m
+}
+
+// Topic sets the message's topic and destination partition ID.
+func (b *KafkaMessageBuilder) Topic(topic *string, partitionID int32) *KafkaMessageBuilder {
+	b.m.TopicPartition = kafka.TopicPartition{
+		Topic:     topic,
+		Partition: partitionID,
+	}
+
+	return b
+}
+
+// Key sets the message's key.
+func (b *KafkaMessageBuilder) Key(key []byte) *KafkaMessageBuilder {
+	b.m.Key = key
+
+	return b
+}
+
+// Payload sets the message's value (payload bytes).
+func (b *KafkaMessageBuilder) Payload(payload []byte) *KafkaMessageBuilder {
+	b.m.Value = payload
+
+	return b
+}
+
+// Header adds a header to the message's headers array.
+func (b *KafkaMessageBuilder) Header(header kafka.Header) *KafkaMessageBuilder {
+	b.m.Headers = append(b.m.Headers, header)
+
+	return b
+}
