4083271f981e9a99448a48aa431c9569498ee3ef
maroon <maroonay@gmail.com>
2021-09-23 16:33:07 +0300

    undo last commit

--- kafka-client/kafka-consumer/kafka_consumer.go
+++ kafka-client/kafka-consumer/kafka_consumer.go
@@ -87,7 +87,7 @@ func createKafkaConsumer() (*kafka.Consumer, []string, error) {
 			"ssl.ca.location":    kafkaclient.GetCertificate(&ca),
 			"client.id":          clientID,
 			"group.id":           clientID,
-			"auto.offset.reset":  "latest",
+			"auto.offset.reset":  "earliest",
 			"enable.auto.commit": "false",
 		})
 		if err != nil {
@@ -101,7 +101,7 @@ func createKafkaConsumer() (*kafka.Consumer, []string, error) {
 		"bootstrap.servers":  hosts,
 		"client.id":          clientID,
 		"group.id":           clientID,
-		"auto.offset.reset":  "latest",
+		"auto.offset.reset":  "earliest",
 		"enable.auto.commit": "false",
 	})
 	if err != nil {
