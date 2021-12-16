6abed6195275b75fa2bc4a65c119e03dab89a6e2
Nir Rozenbaum <nirro@il.ibm.com>
2021-12-16 09:42:46 +0200

    added error wrapping by using fmt.Errorf in certificates manager
    Signed-off-by: Nir Rozenbaum <nirro@il.ibm.com>

--- kafka-client/certificate_manager.go
+++ kafka-client/certificate_manager.go
@@ -4,6 +4,7 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"io/ioutil"
 )
 
@@ -12,21 +13,17 @@ const (
 	certFileLocation = "/opt/kafka/ca.crt"
 )
 
-var (
-	errFailedToAppendCert = errors.New("kafka-certificate-manager: failed to append certificate")
-	errFailedToDecodeCert = errors.New("kafka-certificate-manager: failed to decode certificate")
-	errFailedToWriteCert  = errors.New("kafka-certificate-manager: failed to write certificate")
-)
+var errFailedToAppendCert = errors.New("kafka-certificate-manager: failed to append certificate")
 
 // SetCertificate creates a file with the certificate (PEM) received and registers it in root certification authority.
 func SetCertificate(cert *string) (string, error) {
 	certBytes, err := base64.StdEncoding.DecodeString(*cert)
 	if err != nil {
-		return "", errFailedToDecodeCert
+		return "", fmt.Errorf("kafka-certificate-manager failed to decode certificate - %w", err)
 	}
 
 	if err = ioutil.WriteFile(certFileLocation, certBytes, ownerOnlyRW); err != nil {
-		return "", errFailedToWriteCert
+		return "", fmt.Errorf("kafka-certificate-manager failed to write certificate - %w", err)
 	}
 
 	// Get the SystemCertPool, continue with an empty pool on error
